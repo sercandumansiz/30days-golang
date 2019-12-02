b7978a418a113d91a1e5ec27a649fa10b2d698aa
sdumansiz <sercan.dumansiz@hepsiburada.com>
2019-12-02 11:39:15 +0300

    Day 1 - Hello World & Variables

---
+++ day1/variables/variables.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str = "string"
+	otherString := "otherString"
+	var defaultString string
+
+	fmt.Println(str)
+	fmt.Println(otherString)
+	fmt.Println(defaultString)
+
+	var integer = 5
+	otherInteger := 10
+	var defaultInteger int = 8
+
+	fmt.Println(integer)
+	fmt.Println(otherInteger)
+	fmt.Println(defaultInteger)
+
+	var boolean = false
+	otherBoolean := true
+	var defaultBoolean bool
+
+	fmt.Println(boolean)
+	fmt.Println(otherBoolean)
+	fmt.Println(defaultBoolean)
+}
